98c05a04d2cc4438e1339ff0c2f0dc5aae43f923
S.M <smshafin.me@gmail.com>
2026-01-01 10:35:58 +0600

    Update member IDs

--- lib/constants/memberIds.ts
+++ lib/constants/memberIds.ts
@@ -1,16 +1,16 @@
 export const MEMBER_IDS = [
-    'BB-X9R2-ZT',
-    'BB-K4W5-PM',
-    'BB-Q7L1-VX',
-    'BB-N3B8-RD',
-    'BB-J6F9-KS',
-    'BB-H2M4-TY',
-    'BB-G8S5-QL',
-    'BB-V1C7-NZ',
-    'BB-P5D2-JW',
-    'BB-Z9T4-HX',
-    'BB-A3L6-MR',
-    'BB-S7K1-FP',
+    'AFROredX6Q9K2',
+    'redX7Q9L2M8K4AZ',
+    'redXF92MQA7LK8Z',
+    'redX8KQ7M2ZA9LF',
+    'redXA9ZK2QF8M7L',
+    'redX6MAQ9KZ2F8L',
+    'redXQ82ZLAM9KF7',
+    'redXZ9F7QKA2LM8',
+    'redXL2A8ZMQ79KF',
+    'redXKF9Q82AZ7LM',
+    'redX7ZAFM9QK28L',
+    'redX29LQFZK8AM7',
 ] as const;
 
 export type MemberId = typeof MEMBER_IDS[number];
